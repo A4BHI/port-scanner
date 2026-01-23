eb9ec45d06f31f789cc658944721f9cf4d908918
A4BHI <manthukafc@gmail.com>
2026-01-23 19:08:47 +0530

    hmm

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import (
+	"os"
 
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	godotenv.Load()
+	tgbotapi.NewBotAPI(os.Getenv("BOT_API"))
 }
