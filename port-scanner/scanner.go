d0b3e2b099b67f5bb78bca963d516ee7a3b0beba
A4BHI <manthukafc@gmail.com>
2026-01-24 18:38:02 +0530

    hnn

--- port-scanner/scanner.go
+++ port-scanner/scanner.go
@@ -33,7 +33,7 @@ func ScanPort(target string, updates *tgbotapi.Update) {
 	extraPorts := []string{"8080", "3389", "1443", "3306", "3389", "5900"}
 	go func() {
 		wg.Add(1)
-		wg.Done()
+		defer wg.Done()
 		for _, ports := range extraPorts {
 			address := target + ":" + ports
 			conn, err := net.DialTimeout("tcp", address, 1000*time.Millisecond)
