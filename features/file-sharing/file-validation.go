677e505ccd112e2acee8c72b36fdd2ea72b7d264
A4BHI <manthukafc@gmail.com>
2026-02-16 22:48:55 +0530

    mh

--- features/file-sharing/file-validation.go
+++ features/file-sharing/file-validation.go
@@ -3,11 +3,20 @@ package filesharing
 import tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 
 func ValidateFile(filetype string, fileid string, update *tgbotapi.Update) bool {
+	// switch filetype{
 
-	mime := update.Message.Document.MimeType
-	if mime == "exe" {
-		return false
+	// }
+
+	switch {
+	case update.Message.Document != nil:
+		mime := update.Message.Document.MimeType
+		if mime == "exe" {
+			return false
+		}
+		return true
+
 	}
-	return true
+
+	return false
 
 }
